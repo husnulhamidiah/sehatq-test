dc83964d51b20e6e0dfc8bc35ef8beaa68c8f313
Husnul Anwari <husnulhamidiah@gmail.com>
2019-10-10 09:04:21 +0700

    Fix appoinment validation

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,6 +1,5 @@
 class AppointmentsController < ApplicationController
   def create
-    binding.pry
     return head :unprocessable_entity if !valid?
     
     appointment = Appointment.new
@@ -16,19 +15,32 @@ class AppointmentsController < ApplicationController
   private
 
   def valid?
-    #  check if start_date < end_date
+    now = DateTime.now.in_time_zone
+
+    # check if appointment time is in the past
+    return false if start_date < now || end_date < now
+
+    #  check if start_date > end_date
     return false if start_date > end_date
 
-    # check if aapointment time in weekend
+    # check if apointment time in weekend
     return false if (weekend?(start_date) || weekend?(end_date))
 
-    # check if appointment time covered by doctor schedule
     doctor = Doctor.find params[:doctor_id]
+    # check if appoinment time maximum within 30 minutes before start
+    if (now.all_day.include? start_date)
+      return false if start_date > doctor.start_at.hour - 30.minutes 
+    end
+    # check if appointment time covered by doctor schedule
     return false if !(doctor.start_at.hour..doctor.end_at.hour).cover? start_date.hour..end_date.hour
 
+    # check if doctor has less than 10 appointments
+    count = Appointment.where(doctor_id: params[:doctor_id])
+      .where('start_at > ?', now).count
+    return false if count >= 10
 
     # check if appointment time overlaps
-    appointments = Appointment.where(start_at: start_date.all_day)
+    appointments = Appointment.where(doctor_id: params[:doctor_id], start_at: start_date.all_day)
     return appointments.reduce(true) do |acc, cur|
       acc = false if (start_date..end_date).overlaps? (cur.start_at + 1.second..cur.end_at - 1.second)
       acc
@@ -36,11 +48,11 @@ class AppointmentsController < ApplicationController
   end
 
   def start_date
-    params[:start_at].to_datetime
+    params[:start_at].in_time_zone
   end
 
   def end_date
-    params[:end_at].to_datetime
+    params[:end_at].in_time_zone
   end
 
   def weekend?(date)
